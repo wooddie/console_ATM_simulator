4e21eeda814abdbc250d4ec971ecc3db65e0881d
wooddie <n1ght.d1ck1ns@gmail.com>
2025-09-05 21:46:32 +0500

    Initial Commit

---
+++ console_ATM_simulator/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  console_ATM_simulator
+//
+//  Created by Канапия on 05.09.2025.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
